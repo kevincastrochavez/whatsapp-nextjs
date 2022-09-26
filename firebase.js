f58484e6178d5fc570c8d4b2f6aa55a94f224c1c
Kevin <kevin.castro.chavez0@gmail.com>
2022-09-26 13:43:18 -0600

    setup firebase firestore and file

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import firbase from 'firbase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDVinwFM9K2dMfAG4XF-k3Kr658IWGcYBo',
+  authDomain: 'whatsapp2-3819a.firebaseapp.com',
+  projectId: 'whatsapp2-3819a',
+  storageBucket: 'whatsapp2-3819a.appspot.com',
+  messagingSenderId: '371778043356',
+  appId: '1:371778043356:web:61dd50b570595521e7e319',
+};
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+const auth = app.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
