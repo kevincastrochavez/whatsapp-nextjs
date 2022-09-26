9a50c72aa4ed14e2336076117bcdc48e1b3c4502
Kevin <kevin.castro.chavez0@gmail.com>
2022-09-26 13:16:03 -0600

    installed material-ui icons and some of them

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Avatar, IconButton } from '@mui/material';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import styled from 'styled-components';
 
 function Sidebar() {
   return (
     <Container>
-      <Header></Header>
+      <Header>
+        <UserAvatar />
+
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
     </Container>
   );
 }
@@ -14,3 +28,7 @@ export default Sidebar;
 const Container = styled.div``;
 
 const Header = styled.div``;
+
+const UserAvatar = styled(Avatar)``;
+
+const IconsContainer = styled.div``;
