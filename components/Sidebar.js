ac600317d0c355c3047199187bcba01cdf6f2b58
Kevin <kevin.castro.chavez0@gmail.com>
2022-09-26 13:30:11 -0600

    added more styled components to the sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Avatar, IconButton } from '@mui/material';
+import { Avatar, Button, IconButton } from '@mui/material';
 import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
 import styled from 'styled-components';
 
 function Sidebar() {
@@ -19,6 +20,13 @@ function Sidebar() {
           </IconButton>
         </IconsContainer>
       </Header>
+
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder='Search in chats' />
+      </Search>
+
+      <SidebarButton>Start a new chat</SidebarButton>
     </Container>
   );
 }
@@ -27,8 +35,47 @@ export default Sidebar;
 
 const Container = styled.div``;
 
-const Header = styled.div``;
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
 
-const UserAvatar = styled(Avatar)``;
+const SidebarButton = styled(Button)`
+  width: 100%;
+  /* Increases the priority of the rule */
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
 
 const IconsContainer = styled.div``;
